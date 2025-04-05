9a741e8dcc1d3c0583c88452b0ffdc445da97211
qingy1337 <qxli2@students.everettcc.edu>
2025-04-05 13:00:31 -0700

    Update utils_vllm.py

--- eval/generate_api_answers/utils_vllm.py
+++ eval/generate_api_answers/utils_vllm.py
@@ -7,6 +7,8 @@ from packaging.version import parse as parse_version
 
 IS_OPENAI_V1 = parse_version(openai.__version__) >= parse_version('1.0.0')
 
+SYNTHIA_SYS_PROMPT = "Your role as an assistant involves thoroughly exploring questions through a systematic long thinking process before providing the final precise and accurate solutions. This requires engaging in a comprehensive cycle of analysis, summarizing, exploration, reassessment, reflection, backtracing, and iteration to develop well-considered thinking process. Please structure your response into two main sections: Thought and Solution. In the Thought section, detail your reasoning process using the specified format: <|begin_of_thought|> {thought with steps separated with '\n\n'} <|end_of_thought|> Each step should include detailed considerations such as analisying questions, summarizing relevant findings, brainstorming new ideas, verifying the accuracy of the current steps, refining any errors, and revisiting previous steps. In the Solution section, based on various attempts, explorations, and reflections from the Thought section, systematically present the final solution that you deem correct. The solution should remain a logical, accurate, concise expression style and detail necessary step needed to reach the conclusion, formatted as follows: <|begin_of_solution|> {final formatted, precise, and clear solution} <|end_of_solution|> Now, try to solve the following question through the above guidelines:"
+
 if IS_OPENAI_V1:
     from openai import APIError, APIConnectionError, RateLimitError
 else:
@@ -26,7 +28,7 @@ def get_content(query, base_url, model_name):
         )
     else:
         client = None
-    messages = [{'role': 'user', 'content': query}]
+    messages = [{'role': SYNTHIA_SYS_PROMPT, 'content': query}]
     call_args = dict(
             model=model_name,
             messages=messages,
@@ -81,8 +83,4 @@ if __name__ == "__main__":
     conversation_history = []
     user_input = "Hello!"
     res = get_content(user_input, "http://127.0.0.1:8030/v1", "Tesslate/Synthia-S1-27b")
-    print(f"Response: {res}")
-
-    user_input = "How are you?"
-    res = get_content(user_input, "http://10.77.249.36:8030/v1", "Qwen/QwQ")
     print(f"Response: {res}")
